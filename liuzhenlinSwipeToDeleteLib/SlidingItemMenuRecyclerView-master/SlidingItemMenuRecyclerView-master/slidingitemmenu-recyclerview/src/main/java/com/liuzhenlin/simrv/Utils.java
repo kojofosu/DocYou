797017c96ac5d2ec59ba03a0407af587ef7d531e
kojofosu edue <kojofosube@gmail.com>
2020-08-17 15:09:34 +0000

    bumped gradle version and android library versions

---
+++ liuzhenlinSwipeToDeleteLib/SlidingItemMenuRecyclerView-master/SlidingItemMenuRecyclerView-master/slidingitemmenu-recyclerview/src/main/java/com/liuzhenlin/simrv/Utils.java
@@ -0,0 +1,20 @@
+/*
+ * Created on 11/6/18 5:48 PM.
+ * Copyright © 2018 刘振林. All rights reserved.
+ */
+
+package com.liuzhenlin.simrv;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.ViewCompat;
+
+/**
+ * @author 刘振林
+ */
+public class Utils {
+    public static boolean isLayoutRtl(@NonNull View view) {
+        return ViewCompat.getLayoutDirection(view) == ViewCompat.LAYOUT_DIRECTION_RTL;
+    }
+}
